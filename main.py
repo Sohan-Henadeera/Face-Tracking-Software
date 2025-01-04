aa77afdc486201a65db540047945d0f5ce254bdf
Sohan-Henadeera <sohan.henad@gmail.com>
2025-01-05 10:58:49 +1100

    Face Tracking Software v1 final

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+import mediapipe as mp
+
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+with mp_face_detection.FaceDetection(min_detection_confidence=0.2) as face_detection:
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            break
+
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = face_detection.process(image)
+
+        if results.detections:
+            for detection in results.detections:
+                mp_drawing.draw_detection(frame, detection)
+
+        cv2.imshow('Face Tracker v1', frame)
+
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
